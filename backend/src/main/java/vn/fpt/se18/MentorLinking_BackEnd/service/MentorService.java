a2f25c0d32da8bc98d357611349dd49c16a91d03
hongthuu <thuvh136@gmail.com>
2025-10-30 08:46:48 +0700

    user admin management

--- backend/src/main/java/vn/fpt/se18/MentorLinking_BackEnd/service/MentorService.java
+++ backend/src/main/java/vn/fpt/se18/MentorLinking_BackEnd/service/MentorService.java
@@ -1,10 +1,17 @@
 package vn.fpt.se18.MentorLinking_BackEnd.service;
 
+import vn.fpt.se18.MentorLinking_BackEnd.dto.response.mentor.CountryResponse;
 import vn.fpt.se18.MentorLinking_BackEnd.dto.response.mentor.MentorDetailResponse;
 import vn.fpt.se18.MentorLinking_BackEnd.dto.response.mentor.MentorPageResponse;
+
+import java.util.List;
 
 public interface MentorService {
      MentorPageResponse getAllMentors(String keyword, String sort, int page, int size);
 
      MentorDetailResponse getMentorById(Long id);
+     
+     List<CountryResponse> getMentorCountries(Long mentorId);
+     
+     void updateMentorCountries(Long mentorId, List<Long> countryIds);
 }
