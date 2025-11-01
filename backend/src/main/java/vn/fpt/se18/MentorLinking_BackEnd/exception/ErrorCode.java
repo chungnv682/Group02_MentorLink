f52fd1a7fd814ff1861a7c7058a034bd1e29c316
nguyenbahien123 <nguyenbahien170604@gmail.com>
2025-11-01 21:04:22 +0700

    feat/handle-booking

--- backend/src/main/java/vn/fpt/se18/MentorLinking_BackEnd/exception/ErrorCode.java
+++ backend/src/main/java/vn/fpt/se18/MentorLinking_BackEnd/exception/ErrorCode.java
@@ -19,6 +19,7 @@ public enum ErrorCode {
     JWT_MALFORMED("10", "JWT token is malformed", HttpStatus.UNAUTHORIZED),
     JWT_UNSUPPORTED("11", "JWT token is unsupported", HttpStatus.UNAUTHORIZED),
     JWT_ILLEGAL_ARGUMENT("12", "JWT token is invalid", HttpStatus.UNAUTHORIZED),
+    EMAIL_INVALID("13", "EMAIL_INVALID", HttpStatus.BAD_REQUEST),
     ;
     private final String code;
     private final String message;
