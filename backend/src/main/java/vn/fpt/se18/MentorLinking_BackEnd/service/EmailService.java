4362ea0af7e0f17ff69687aa8c9300a7a3068357
nguyenbahien123 <nguyenbahien170604@gmail.com>
2025-11-03 22:32:56 +0700

    update2: booking

--- backend/src/main/java/vn/fpt/se18/MentorLinking_BackEnd/service/EmailService.java
+++ backend/src/main/java/vn/fpt/se18/MentorLinking_BackEnd/service/EmailService.java
@@ -2,15 +2,16 @@ package vn.fpt.se18.MentorLinking_BackEnd.service;
 
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.List;
 
 public interface EmailService {
-    public void sendConfirmBooking(String to, String subject, String studentName, String mentorName, String service, LocalDate date, Long timeStart, Long timeEnd, String linkMeeting);
+    public void sendConfirmBooking(String to, String subject, String studentName, String mentorName, String service, LocalDate date, List<Long[]> bookingTimes, String linkMeeting);
 
     public void sendRequestFeedback(String to, String subject, String body);
 
     public void sendRejectBooking(String to, String subject, String studentName, String reason);
 
-    public void sendRejectBooking1(String to, String subject, String studentName, String mentorName, String service, LocalDate date, Long timeStart, Long timeEnd, String reason);
+    public void sendRejectBooking1(String to, String subject, String studentName, String mentorName, String service, LocalDate date, List<Long[]> bookingTimes, String reason);
 
     public void send(String to, String subject, String text);
 }
