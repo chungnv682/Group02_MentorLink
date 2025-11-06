21a7b50c4553d55a766cddca88dcc61dc36b2ec1
nguyenbahien123 <nguyenbahien170604@gmail.com>
2025-11-06 17:32:56 +0700

    mentor service, info management

--- frontend/src/services/mentor/index.js
+++ frontend/src/services/mentor/index.js
@@ -1,3 +1,4 @@
 // Export all mentor services
 export { default as ScheduleService } from './ScheduleService';
 export { default as TimeSlotService } from './TimeSlotService';
+export { default as MentorServiceCrud } from './MentorServiceCrud';
